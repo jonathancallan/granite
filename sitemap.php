3adb9b1a7ef9ed6f5abcd02718d5c08a01c6948d
Jonathan Callan <jonathan.callan@granite.ie>
2018-09-28 23:04:32 +0100

    Files tidy up.

---
+++ sitemap.php
@@ -0,0 +1,227 @@
+<?php
+$metaTitle = "Granite Digital - Privacy Policy";
+$metaDescription = "Granite Digital.";
+$metaKeywords = "Granite Digital";
+$ogUrl = "https://www.granite.ie/privacy-policy";
+?>
+  <?php require 'inc/head.php'; ?>
+
+  <body class="services standard">
+    <div class="hero-background"> <svg id="angle-bar" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 639.26 414.13">
+      <polygon class="primary" points="0 280.89 133.24 414.13 547.37 0 280.89 0 0 280.89"/><polygon class="secondary" points="388 191.6 417.83 221.44 639.26 0 579.61 0 388 191.6"/></svg> </div>
+    <?php require 'inc/header.php'; ?>
+    <main>
+      <!-- Hero banner -->
+      <section class="hero">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col offset-sm-3">
+              <nav class="backlink"> <a href="javascript:history.go(-1)"><img src="./img/arrow.svg" alt="Services"></a> </nav>
+              <h1>Sitemap</h1> </div>
+          </div>
+        </div>
+      </section>
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="offset-sm-3 col-sm-7 col-xl-6">
+                <h3>Main Navigation</h3>
+              <ul><li><span class="midd"></span>
+					<a href="https://www.granite.ie/home" title="Granite Digital is a Cork &amp; Dublin based digital agency with expertise in Digital Marketing providing clients with Website Development, Web Design, SEO, PPC, email marketing and Social Media as well as online software &amp; application development">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Home" border="0" align="absmiddle">
+						Home
+					</a>
+				</li><li><span class="midd"></span>
+					<a href="https://www.granite.ie/what-we-do" title="Learn about the digital marketing and website development team at Granite Digital. Experts in SEO and website design.">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="What We Do" border="0" align="absmiddle">
+						What We Do
+					</a>
+				<ul><li><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/design" title="At Granite Digital we offer an extensive range of web design services to meet all your requirements.  
+">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Design" border="0" align="absmiddle">
+						Design
+					</a>
+				<ul><li><span class="vert"></span><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/responsive-design" title="Maximise your online presence with Granite Digital's cutting edge responsive Web design for businesses throughout Ireland. Our mobile friendly website development and design ensures your website is optimised for mobile devices.">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Responsive Design" border="0" align="absmiddle">
+						Responsive Design
+					</a>
+				</li><li><span class="vert"></span><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/website-design" title="Granite Digital provide cutting edge Web design to help businesses of all sizes throughout Ireland to enhance their online presence.">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Website Design" border="0" align="absmiddle">
+						Website Design
+					</a>
+				</li><li><span class="vert"></span><span class="vert"></span><span class="last"></span>
+					<a href="https://www.granite.ie/branding-logo-design-and-corporate-identity" title="We can help you transform your strategic thinking and logo design into an overall brand identity for your company.  ">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Branding, Logo Design and Corporate Identity" border="0" align="absmiddle">
+						Brand Identity
+					</a>
+				</li></ul></li><li><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/web-development" title="Granite Digital are experts in website development and design. At Granite we are passionate about development of cutting edge websites and online software for businesses of all sizes.
+">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Development" border="0" align="absmiddle">
+						Development
+					</a>
+				<ul><li><span class="vert"></span><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/content-management-systems-_cm" title="Content Management Systems (CMS system) gives users the ability to edit and publish content live on their website making it a very important tool to manage your own website.">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Content Management Systems (CMS)" border="0" align="absmiddle">
+						Content Management Systems (CMS)
+					</a>
+				</li><li><span class="vert"></span><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/custom-web-app-development" title="With over ten years experience in developing custom web based applications for customers, Granite Digital Agency develops web apps for a wide range of companies. ">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Custom Web App Development" border="0" align="absmiddle">
+						Custom Web App Development
+					</a>
+				</li><li><span class="vert"></span><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/mobile-apps" title="We develop Apps for Apple (iOS) and Android powered devices including mobile, smartphone and tablet devices.">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Mobile Apps" border="0" align="absmiddle">
+						Mobile Apps
+					</a>
+				</li><li><span class="vert"></span><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/intranet-extranet-development" title="Granite Digital develop secure and scalable Intranet and Extranet solutions for many leading businesses in Ireland.">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Intranet / Extranet Development" border="0" align="absmiddle">
+						Intranet / Extranet Development
+					</a>
+				</li><li><span class="vert"></span><span class="vert"></span><span class="last"></span>
+					<a href="https://www.granite.ie/bespoke-projects" title="Contact us today with details of the software system you would like developed. At Granite Digital we love a challenge!">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Bespoke Software Projects" border="0" align="absmiddle">
+						Bespoke Projects
+					</a>
+				</li></ul></li><li><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/digital-marketing" title="Granite Digital are a digital marketing agency based in Cork who combine SEO, Social Media Marketing, Pay Per Click (PPC) and web development to build awareness of your brand, product or service online.">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Digital Marketing" border="0" align="absmiddle">
+						Digital Marketing
+					</a>
+				<ul><li><span class="vert"></span><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/search-engine-optimisation-seo" title="Granite Digital are a successful Search Engine Optimisation (SEO) Agency in Cork, Dublin and Galway. Our objective is to achieve maximum visibility for our clients in the Search Engine Results Pages">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Search Engine Optimisation " border="0" align="absmiddle">
+						Search Engine Optimisation 
+					</a>
+				</li><li><span class="vert"></span><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/search-marketing-audit" title="Search engines are a primary source of information for millions of people. We offer search marketing audits that can help you establish the search engine strength of your website.">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Search Marketing Audit" border="0" align="absmiddle">
+						Search Marketing Audit
+					</a>
+				</li><li><span class="vert"></span><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/search-engine-marketing-sem" title="Granite Digital are at the cutting edge of search engine marketing (SEM), employing the latest techniques and technologies to build awareness of your brand, product or service online.">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Search Engine Marketing" border="0" align="absmiddle">
+						Search Engine Marketing
+					</a>
+				</li><li><span class="vert"></span><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/pay-per-click-management-ppc" title="Pay Per Click, also known as PPC, is an internet advertising model used for precise targeting in search engines using platforms such as Google AdWords. Efficient management can yield strong returns. 
+">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Pay Per Click (PPC) Management" border="0" align="absmiddle">
+						Pay Per Click Management
+					</a>
+				</li><li><span class="vert"></span><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/blogging" title="Blogging is useful for online marketing and in particular search engine optimisation and in driving traffic and links to your website. At Granite we can help design your blog as well as assist with content.">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Blogging" border="0" align="absmiddle">
+						Blogging
+					</a>
+				</li><li><span class="vert"></span><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/email-marketing" title="With e-newsletters you can interact directly with your customers in the most cost effective way. It also provides effective delivery and memorability. We can design and implement your email marketing campaign for you.">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Email Marketing" border="0" align="absmiddle">
+						Email Marketing
+					</a>
+				</li><li><span class="vert"></span><span class="vert"></span><span class="last"></span>
+					<a href="https://www.granite.ie/display-advertising" title="We have huge and broad experience of managing online display advertising campaigns for varied clients. Unlike traditional advertising, digital display media involves programmatic advertising to automate the decision-making process of digital media buying.">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Digital Media Buying / Display Advertising" border="0" align="absmiddle">
+						Digital Media Buying / Display Advertising
+					</a>
+				</li></ul></li><li><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/training">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Training" border="0" align="absmiddle">
+						Training
+					</a>
+				<ul><li><span class="vert"></span><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/seo-workshops" title="An SEO workshop to gain a basic understanding of search engine optimisation to enhance and improve the visibility of your website in organic search results, delivering visible results for your business.">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="SEO Workshops in Cork and Dublin" border="0" align="absmiddle">
+						SEO Workshops Cork and Dublin
+					</a>
+				</li><li><span class="vert"></span><span class="vert"></span><span class="last"></span>
+					<a href="https://www.granite.ie/one-to-one-seo-training" title="Private 3-4 hour SEO Training tailored to your needs and company's website which includes an appraisal of your website. Time and date chosen by you. Cork, Dublin or elsewhere by arrangement.
+">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="One to One SEO Training" border="0" align="absmiddle">
+						One to One SEO Training
+					</a>
+				</li></ul></li><li><span class="vert"></span><span class="last"></span>
+					<a href="https://www.granite.ie/cloud-email" title="Having problems with your email service? Cloud based email is the answer. email is backed up and stored securely in the cloud and can be accessed anywhere you want.
+">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Cloud Email" border="0" align="absmiddle">
+						Cloud Email
+					</a>
+				</li></ul></li><li><span class="midd"></span>
+					<a href="https://www.granite.ie/who-we-are" title="Granite Digital are one of Ireland’s leading Digital Marketing companies, with a proven record for delivering results and new business for our clients through search engine marketing and search engine friendly website design.">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Who We Are" border="0" align="absmiddle">
+						Who We Are
+					</a>
+				</li><li><span class="midd"></span>
+					<a href="https://www.granite.ie/portfolio" title="Granite digital work with some amazing clients throughout Ireland and the UK, here are some of the projects we have worked on across web design, development and digital marketing.">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Our Work" border="0" align="absmiddle">
+						Our Work
+					</a>
+				<ul><li><span class="vert"></span><span class="last"></span>
+					<a href="https://www.granite.ie/websites">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Websites" border="0" align="absmiddle">
+						Websites
+					</a>
+				</li></ul></li><li><span class="midd"></span>
+					<a href="https://www.granite.ie/news" title="Stay up-to-date with the latest news and trends from the world of digital marketing and web design from the team at Granite Digital in Cork. ">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="News &amp; Digital Marketing Trends" border="0" align="absmiddle">
+						News
+					</a>
+				</li><li><span class="midd"></span>
+					<a href="https://www.granite.ie/careers" title="SEO Specialist required for rapidly growing digital marketing agency with offices in Cork &amp; Dublin">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Careers" border="0" align="absmiddle">
+						Careers
+					</a>
+				<ul><li><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/seo-manager" title="At Granite Digital we have a fantastic opportunity for an experienced SEO Specialist to join our expanding team. We are looking for someone with a passion for all things SEO combined with drive, enthusiasm and a real passion for achieving results.">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="SEO Manager Job" border="0" align="absmiddle">
+						SEO Manager
+					</a>
+				</li><li><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/accounts-assistant">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Accounts Assistant" border="0" align="absmiddle">
+						Accounts Assistant
+					</a>
+				</li><li><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/dotnet-web-developer">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt=".Net Web Developer" border="0" align="absmiddle">
+						.Net Web Developer
+					</a>
+				</li><li><span class="vert"></span><span class="midd"></span>
+					<a href="https://www.granite.ie/senior-digital-project-manager">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Senior Digital Project Manager" border="0" align="absmiddle">
+						Senior Digital Project Manager
+					</a>
+				</li><li><span class="vert"></span><span class="last"></span>
+					<a href="https://www.granite.ie/ui-ux-designer" title="Do you want to join the fastest growing Digital agency in Ireland as a UI / UX Designer?">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="UI / UX Designer" border="0" align="absmiddle">
+						UI / UX Designer
+					</a>
+				</li></ul></li><li><span class="midd"></span>
+					<a href="https://www.granite.ie/contact-us">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Contact Us" border="0" align="absmiddle">
+						Contact Us
+					</a>
+				<ul><li><span class="vert"></span><span class="last"></span>
+					<a href="https://www.granite.ie/customer-support">
+						<img src="dccom/components/TWSitemap/filetypes/unknown.gif" height="13" hspace="0" alt="Customer support" border="0" align="absmiddle">
+						Customer support
+					</a>
+				</li></ul></li></ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    <div class="footer-cta bg-white">
+      <div class="box">
+        <h2>Let us help your Digital Marketing</h2>
+        <p>Contact us today <a href="tel:+35318512800">+ 353 (1) 6337979</a></p>
+      </div>
+    </div>
+    <footer class="bg-white">
+      <?php require 'inc/footer.php'; ?>
