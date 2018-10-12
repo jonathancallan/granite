13b5296fdf5644ba35c8647af755d1dbc94be834
Jonathan Callan <jonathan.callan@granite.ie>
2018-10-12 09:59:29 +0100

    Update news article.

--- news/granite-digital-acquire-webtrade-to-create-5m-agency.php
+++ news/granite-digital-acquire-webtrade-to-create-5m-agency.php
@@ -1,49 +1,96 @@
 <?php
 $basePath = $_SERVER['DOCUMENT_ROOT'];
-$metaTitle = "Granite digital acquire Webtrade to create €5m digital agency";
-$metaDescription = "Granite digital acquire Webtrade to create €5m digital agency. Granite Digital are one of Ireland’s leading Digital Marketing companies, with a proven record for delivering results and new business for our clients through search engine marketing and search engine friendly website design";
+$metaTitle = "Granite acquire Dublin based Webtrade to create €5m digital agency";
+$metaDescription = "Deal establishes Granite Digital as the largest independent digital agency in Ireland. Granite Digital, acquire the assets of multiple Fast 50 winner, Webtrade, to further expand its client base and talent pool in Dublin";
 $ogUrl = "https://www.granite.ie/news/granite-digital-acquire-webtrade-to-create-5m-agency/";
+$ogImage = "https://www.granite.ie/img/Granite-Digital-002.jpg";
 ?>
 <?php include $basePath."/inc/head.php" ?>
 
 
 <body class="about standard">
-    <div class="hero-background">
-        <svg id="angle-bar" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 639.26 414.13">
+  <div class="hero-background">
+    <svg id="angle-bar" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 639.26 414.13">
             <polygon class="primary" points="0 280.89 133.24 414.13 547.37 0 280.89 0 0 280.89"/>
             <polygon class="secondary" points="388 191.6 417.83 221.44 639.26 0 579.61 0 388 191.6"/>
         </svg>
-    </div>
-    <?php include $basePath."/inc/header.php" ?>
+  </div>
+  <?php include $basePath."/inc/header.php" ?>
 
-    <main>
-        <!-- Hero banner -->
-        <section class="hero">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col offset-sm-3">
-                        <nav class="backlink"> <a href="../who-we-are.php"><img src="../img/arrow.svg" alt="Services"></a> </nav>
-                        <h1>Granite digital acquire Webtrade to create €5m digital agency</h1>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="offset-sm-3 col-sm-7 col-xl-6">
-                        <!-- Content -->
-                    </div>
-                </div>
-            </div>
-        </section>
-    </main>
-    <div class="footer-cta bg-white">
-        <div class="box">
-            <h2>Let us help your Digital Marketing</h2>
-            <p>Contact us today <a href="tel:+35318512800">+ 353 (1) 6337979</a>
-            </p>
+  <main>
+    <!-- Hero banner -->
+    <section class="hero">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col offset-sm-3">
+            <nav class="backlink"> <a href="/"><img src="../img/arrow.svg" alt="Home"></a> </nav>
+            <h1>Granite digital acquire Webtrade to create €5m digital agency</h1>
+            <p>Deal establishes Granite Digital as the largest independent digital agency in Ireland. Granite Digital, acquire the assets of multiple Fast 50 winner, Webtrade, to further expand its client base and talent pool in Dublin.</p>
+          </div>
         </div>
+      </div>
+    </section>
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="offset-sm-3 col-sm-7 col-xl-6">
+            <article class="news-item">
+              <p class="meta text-muted"><span class="author">By: Robert Carpenter |</span> Posted on: 27 Jul 2018</p>
+
+
+              <p>With this deal, Granite Digital has established itself as the largest digital agency in the country with combined turnover in excess of €5m. With a team of over 60 professionals, Granite continues to expand its services
+                nationwide. The company already have offices in Cork, Dublin and Galway and are on schedule to open a new office in Belfast for Q1 2019.</p>
+
+              <p>
+                <img src="../img/Granite-Digital-002.jpg" alt="Granite acquire Webtrade to create €5m agency">
+							</p>
+
+                <p>Offering a wide range of digital services to clients throughout the country - ranging from strategy, consultancy, UX, web design, digital marketing, web development, secure email, managed hosting, Branding, website support,
+                  analytics and training - Granite Digital is on target to grow its business to €10m turnover, creating 40 new jobs growing their workforce to over 100 professionals.</p>
+
+                <p>Expansion is nothing new for Granite Digital as the company has steadily grown to be the largest independent digital agency in Ireland. Previously, Granite completed the acquisition of MediaOne - a Deloitte Fast 50 company for
+                  eight consecutive years prior to that deal. They also completed the takeover of Enhance Multimedia, one of the market leaders in the digital sector since its beginning in 2001. Granite was founded by Partners, Conor Buckley, Robert
+                  Carpenter, Seamus White and Ger O’Shea in 2008 who were subsequently joined by Gary Corcoran and Sean Byrne.</p>
+
+                <p>Chaired by Alf Smiddy, Granite provides a full service web agency delivered by a talented and experienced team. In just under ten years, the original team of five will have expanded to more than 60 following this deal.</p>
+
+                <p>Ronan Murray of Deloitte corporate finance supported Granite on this strategic transaction.</p>
+
+                <p>Speaking about the deal, Granite Digital CEO, Conor Buckley stated:</p>
+
+                <blockquote>
+                  <p>“This is fantastic news for both businesses. We continue to accelerate growth of our business and with this deal we have established ourselves as the largest agency in Ireland. We are delighted to get the opportunity to work
+                    with Webtrade’s existing customers and to make new services available to customers of both businesses. We are very much looking forward to working with the team at Webtrade. ”</p>
+                </blockquote>
+
+                <p>Webtrade was established in 1998&nbsp;and has provided digital services from its head office in Rathfarnham, County Dublin, for all of those years. The wealth of experience the staff of Webtrade bring to the table will be of huge
+                  benefit to Granite Digital now and in the future. Like MediaOne, Webtrade has also been a Deloitte Fast 50 company for eight years in a row.</p>
+
+                <p>Commenting on the deal Webtrade’s owner said:</p>
+
+                <blockquote>
+                  <p>“In the year we celebrate 20 years in the industry, we are delighted to come onboard with Granite Digital to bring two great businesses together and to develop a business that can accelerate to 100 staff across Ireland and the
+                    UK in the next 18-24 months.&nbsp;This deal will cement the core service offerings of both businesses and allow us to offer all clients an expanded suite of services in the digital space. We very much look forward to working
+                    together and are already on our way to becoming one team.”</p>
+                </blockquote>
+
+                <p>Granite Digital continue to win exciting new clients including AON, Davy, Dalata Hotel Group, eir, Audi, UCD, Shannon Airport, Enterprise Ireland and The Cork Chamber. The company is also seeking further new acquisitions which are
+                  currently in due diligence in Ireland and the United Kingdom.</p>
+
+                <p>This is a momentous step for Granite Digital as the company looks to seal its place at the top of the ladder in Ireland’s digital services sector.</p>
+
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <div class="footer-cta bg-white">
+    <div class="box">
+      <h2>Let us help your Digital Marketing</h2>
+      <p>Contact us today <a href="tel:+35318512800">+ 353 (1) 6337979</a>
+      </p>
     </div>
-    <footer class="bg-white">
-        <?php include $basePath."/inc/footer.php" ?>
+  </div>
+  <footer class="bg-white">
+    <?php include $basePath."/inc/footer.php" ?>
