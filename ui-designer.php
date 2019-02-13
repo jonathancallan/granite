f2c5d0fa5da21642047936ba32331a0ab43a54fd
Jonathan Callan <jonathan.callan@granite.ie>
2019-02-13 11:02:31 +0000

    Updated job posts

---
+++ ui-designer.php
@@ -0,0 +1,99 @@
+<?php
+$metaTitle = "Granite Digital - UI Designer";
+$metaDescription = "Granite Digital.";
+$metaKeywords = "Granite Digital";
+$ogUrl = "https://www.granite.ie/ui-designer";
+?>
+  <?php require 'inc/head.php'; ?>
+
+  <body class="careers standard">
+    <div class="hero-background">
+      <svg id="angle-bar" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 639.26 414.13">
+      <polygon class="primary" points="0 280.89 133.24 414.13 547.37 0 280.89 0 0 280.89"/><polygon class="secondary" points="388 191.6 417.83 221.44 639.26 0 579.61 0 388 191.6"/></svg>
+    </div>
+    <?php require 'inc/header.php'; ?>
+    <main>
+      <!-- Hero banner -->
+      <section class="hero">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col offset-sm-3">
+              <nav class="backlink">
+                <a href="./careers.php"><img src="./img/arrow.svg" alt="Services"></a>
+              </nav>
+              <h1>UI Designer</h1>
+              <p>We are looking for a passionate and super-talented UI Designer who is able to create amazing user experiences. If you have have an eye for clean, well structured design and possess superior UI skills, then we want to hear from you.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="offset-sm-3 col-sm-7 col-xl-6">
+              <h2>Role Overview</h2>
+
+              <p><strong>Location: </strong>Dublin City Centre<br />
+<strong>Salary: </strong>Negotiable<br />
+<strong>Role: </strong>Permanent full-time</p>
+
+<p>The design team at Granite is expanding and we’re looking forward to executing on our strategy for 2019. We have recently added several high profile clients to our portfolio and are excited about delivering award winning design experiences for them.</p>
+
+<p>Our vision for this role is that you will bring additional creative flare to the team. Working with the Creative Director, you will be given the freedom to explore exciting design solutions that will raise the profile of both Granite and yourself.</p>
+
+<p>We’d love you to join us.</p>
+
+<h2>Responsibilities</h2>
+
+<ul>
+<li>Be a great designer. Obviously.</li>
+<li>Understanding of basic front-end languages: HTML5, CSS3 Javascript.</li>
+<li>Ability to present your designs and sell your solutions.</li>
+<li>Be open to receiving objective criticism and improving upon it.</li>
+<li>Solid experience in creating wireframes, storyboards, user flows, process flows and site maps. It’s is a design role but it’s important you know this stuff.</li>
+<li>Own all facets of design: interaction, visual, workflow, prototyping.</li>
+<li>Lead workshops and sprints that include PMs and Engineers.</li>
+<li>Create and implement beautiful mockups, wireframes and prototypes.</li>
+<li>Have strong opinions and the confidence to share them with the team.</li>
+<li>Contribute to the Granite design culture as we continue to grow.</li>
+</ul>
+
+<h2>Requirements</h2>
+
+<ul>
+	<li>Minimum 3 years experience in a digital agency.</li>
+	<li>Portfolio of recent work samples for web and mobile applications.</li>
+</ul>
+<p>We’ll work out the rest if you get past the first two.</p>
+
+<h2>Who are Granite?</h2>
+<p>We’re Ireland’s largest digital agency with a team of over 60 people, 20 of whom are based in our Temple Bar office in Dublin City Centre. Our head office is based in Cork. For the last 3 years we have been listed in the Deloitte Fast 50.</p>
+<h3>We offer</h3>
+<ul>
+<li>Fast-paced, friendly environment.</li>
+<li>Remote working</li>
+<li>Open culture</li>
+</ul>
+
+<h3>HOW TO APPLY</h3>
+
+<p>Please submit your CV and cover letter outlining why you are suitable for this role. Email <strong>careers@[our domain]</strong></p>
+
+<p>Ref: UI Designer</p>
+
+
+
+
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    <div class="footer-cta bg-white">
+      <div class="box">
+        <h2>Let us help your Digital Marketing</h2>
+        <p>Contact us today <a href="tel:+35318512800">+ 353 (1) 6337979</a></p>
+      </div>
+    </div>
+    <footer class="bg-white">
+      <?php require 'inc/footer.php'; ?>
