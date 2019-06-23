fe2e3c103e16f48eb6339580ffb29fa78333875b
Jonathan Callan <jonathan.callan@granite.ie>
2019-06-23 21:19:04 +0100

    Grabbed files from server. Gary's changes.

--- contact-us.php
+++ contact-us.php
@@ -7,77 +7,70 @@ $ogUrl = "https://www.granite.ie/contact-us";
 <?php require 'inc/head.php'; ?>
 
 <body class="services standard">
-<div class="hero-background">
-  <svg id="angle-bar" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 639.26 414.13">
-    <polygon class="primary" points="0 280.89 133.24 414.13 547.37 0 280.89 0 0 280.89"/>
-    <polygon class="secondary" points="388 191.6 417.83 221.44 639.26 0 579.61 0 388 191.6"/>
-  </svg>
-</div>
-<?php require 'inc/header.php'; ?>
-<main>
-<!-- Hero banner -->
-<section class="hero">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col offset-sm-3">
-        <nav class="backlink"> <a href="javascript:history.go(-1)"><img src="./img/arrow.svg" alt="Services"></a> </nav>
-        <h1>Contact Us.</h1>
-        <p>Dublin, Cork and Galway.</p>
-      </div>
+    <div class="hero-background">
+        <svg id="angle-bar" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 639.26 414.13">
+            <polygon class="primary" points="0 280.89 133.24 414.13 547.37 0 280.89 0 0 280.89"/>
+            <polygon class="secondary" points="388 191.6 417.83 221.44 639.26 0 579.61 0 388 191.6"/>
+        </svg>
     </div>
-  </div>
-</section>
-<section class="content">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="offset-sm-3 col">
-        <?php include 'inc/contact-form-captcha.php' ?>
-      </div>
-    </div>
-  </div>
-</section>
-<section class="content">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="offset-sm-3 col">
-        <h2 style="margin-bottom: 0;">Offices</h2>
-      </div>
-    </div>
-    <div class="row">
-      <div class="offset-sm-3 col-xs-12 col-sm">
-        <h4>Cork</h4>
-        <p>Granite Digital <br>
-          First Floor,<br>
-          City Quarter,<br>
-          Lapps Quay, Cork</p>
-        <p><a href="tel:+353212427890">+353 (0)21 242 7890</a> <br>
-          <a href="mailto: info@granite.ie">info@granite.ie</a> <br>
-        </p>
-      </div>
-      <div class="col-xs-12 col-sm">
-        <h4>Dublin</h4>
-        <p>25 Temple Lane South,<br>
-          Temple Bar,<br>
-          Dublin 2,<br>
-          Ireland</p>
-        <p><a href="tel:+35318512800">+ 353 (0)1 6337979</a> <br>
-          <a href="mailto: info@granite.ie">info@granite.ie</a> <br>
-        </p>
-      </div>
-      <div class="col-xs-12 col-sm">
-        <h4>Galway</h4>
-        <p><a href="tel:+353091 511457 ">+ 353 (0)91 511457 </a> <br>
-          <a href="mailto: info@granite.ie">info@granite.ie</a> <br>
-        </p>
-      </div>
-    </div>
-  </div>
-</section>
-<div class="footer-cta bg-white">
-  <div class="box">
-    <h2>Let us help your Digital Marketing</h2>
-    <p>Contact us today <a href="tel:+35318512800">+ 353 (1) 6337979</a> </p>
-  </div>
-</div>
-<footer class="bg-white">
-  <?php require 'inc/footer.php'; ?>
+    <?php require 'inc/header.php'; ?>
+    <main>
+        <!-- Hero banner -->
+        <section class="hero">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col offset-sm-3">
+                        <nav class="backlink"> <a href="javascript:history.go(-1)"><img src="./img/arrow.svg" alt="Services"></a> </nav>
+                        <h1>Contact Us.</h1>
+                        <p>Dublin, Cork and Galway.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="content">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="offset-sm-3 col">
+                <?php include 'inc/contact-form-captcha.php' ?>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                <div class="offset-sm-3 col">
+                <h2 style="margin-bottom: 0;">Offices</h2>
+                </div>
+            </div>
+                <div class="row">
+
+                    <div class="offset-sm-3 col-xs-12 col-sm">
+                        <h4>Cork</h4>
+                        <p>Granite Digital <br> First Floor,<br> City Quarter,<br> Lapps Quay, Cork</p>
+                        <p><a href="tel:+353212427890">+353 (0)21 242 7890</a> <br>
+                            <a href="mailto: info@granite.ie">info@granite.ie</a> <br> </p>
+                    </div>
+                    <div class="col-xs-12 col-sm">
+                        <h4>Dublin</h4>
+                        <p>25 Temple Lane South,<br>Temple Bar,<br>Dublin 2,<br>Ireland</p>
+                        <p><a href="tel:+35318512800">+ 353 (0)1 6337979</a> <br>
+                            <a href="mailto: info@granite.ie">info@granite.ie</a> <br> </p>
+                    </div>
+                    <div class="col-xs-12 col-sm">
+                        <h4>Galway</h4>
+                        <p><a href="tel:+353091 511457 ">+ 353 (0)91 511457 </a> <br>
+                            <a href="mailto: info@granite.ie">info@granite.ie</a> <br> </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="footer-cta bg-white">
+            <div class="box">
+                <h2>Let us help your Digital Marketing</h2>
+                <p>Contact us today <a href="tel:+35318512800">+ 353 (1) 6337979</a>
+                </p>
+            </div>
+        </div>
+        <footer class="bg-white">
+            <?php require 'inc/footer.php'; ?>
--- inc/footer.php
+++ inc/footer.php
@@ -3,10 +3,14 @@
     <div class="col-6 col-sm-2 offset-sm-1">
       <h4>Company</h4>
       <ul>
-        <li><a href="./contact-us">Contact Us</a> </li>
-        <li><a href="./privacy-policy">Privacy Policy</a> </li>
-        <li><a href="./cookie-policy">Cookie Policy</a> </li>
-        <li><a href="./testimonials">Testimonials</a> </li>
+        <li><a href="./contact-us">Contact Us</a>
+        </li>
+        <li><a href="./privacy-policy">Privacy Policy</a>
+        </li>
+        <li><a href="./cookie-policy">Cookie Policy</a>
+        </li>
+        <li><a href="./testimonials">Testimonials</a>
+        </li>
         <!--       <li><a href="./sitemap">Sitemap</a>
                 </li> -->
       </ul>
@@ -15,45 +19,55 @@
       <nav>
         <h4>Services</h4>
         <ul>
-          <li><a href="./design">Web Design</a> </li>
-          <li><a href="./web-development">Web Development</a> </li>
-          <li><a href="./digital-marketing">Digital Marketing</a> </li>
-          <li><a href="./search-engine-optimisation-seo">SEO Consultants</a> </li>
+          <li><a href="./design">Web Design</a>
+          </li>
+          <li><a href="./web-development">Web Development</a>
+          </li>
+          <li><a href="./digital-marketing">Digital Marketing</a>
+          </li>
+          <li><a href="./search-engine-optimisation-seo">SEO Consultants</a>
+          </li>
         </ul>
       </nav>
     </div>
-    <div class="col-6 col-sm-2"> 
-      <!-- need to use Schema.org --> 
+    <div class="col-6 col-sm-2">
+      <!-- need to use Schema.org -->
       <span itemscope itemtype="http://schema.org/PostalAddress">
-      <h4 itemprop="addressLocality">Cork</h4>
-      <p itemprop="streetAddress">First Floor,<br>
-        City Quarter,<br>
-        Lapps Quay, Cork</p>
+        <h4 itemprop="addressLocality">Cork</h4>
+        <p itemprop="streetAddress">First Floor,<br> City Quarter,<br> Lapps Quay, Cork</p>
       </span>
+
       <ul itemscope itemtype="https://schema.org/ContactPoint">
         <li class="telephone"><a href="tel:+353212427890" itemprop="telephone">+353 (0)21 2427890 </a> </li>
-        <li class="email"><a href="mailto: info@granite.ie" itemprop="email">info@granite.ie</a> </li>
+        <li class="email"><a href="mailto: info@granite.ie" itemprop="email">info@granite.ie</a>
+        </li>
         <li class="map"><a href="https://www.google.ro/maps/place/51%C2%B053'56.2%22N+8%C2%B027'51.1%22W/@51.8989596,-8.4667035,17z/data=!4m5!3m4!1s0x0:0x0!8m2!3d51.898953!4d-8.464193?hl=en" target="_blank">View Map</a> </li>
       </ul>
     </div>
-    <div class="col-6 col-sm-2"> <span itemscope itemtype="http://schema.org/PostalAddress">
-      <h4 itemprop="addressLocality">Dublin</h4>
-      <p itemprop="streetAddress">25 Temple Lane South,<br>
-        Temple Bar,<br>
-        Dublin 2</p>
+    <div class="col-6 col-sm-2">
+      <span itemscope itemtype="http://schema.org/PostalAddress">
+        <h4 itemprop="addressLocality">Dublin</h4>
+        <p itemprop="streetAddress">25 Temple Lane South,<br> Temple Bar,<br> Dublin 2</p>
       </span>
+
       <ul itemscope itemtype="https://schema.org/ContactPoint">
         <li class="telephone"><a href="tel:+35318512800" itemprop="telephone">+353 (0)1 6337979</a> </li>
-        <li class="email"><a href="mailto: info@granite.ie" itemprop="email">info@granite.ie</a> </li>
+        <li class="email"><a href="mailto: info@granite.ie" itemprop="email">info@granite.ie</a>
+        </li>
         <li class="map"><a href="https://www.google.ie/maps/place/enhance.ie/@53.33333,-6.252179,17z/data=!3m1!4b1!4m5!3m4!1s0x48671284f0e3d8f5:0x9de8e274491c82be!8m2!3d53.33333!4d-6.25223" target="_blank">View Map</a> </li>
       </ul>
     </div>
-    <div class="col-6 col-sm-2"> <span itemscope itemtype="http://schema.org/PostalAddress">
-      <h4 itemprop="addressLocality">Galway</h4>
+    <div class="col-6 col-sm-2">
+      <span itemscope itemtype="http://schema.org/PostalAddress">
+        <h4 itemprop="addressLocality">Galway</h4>
       </span>
+
       <ul itemscope itemtype="https://schema.org/ContactPoint">
         <li class="telephone"><a href="tel:+353091 511457" itemprop="telephone">+353 (0)91 511457 </a> </li>
-        <li class="email"><a href="mailto: info@granite.ie" itemprop="email">info@granite.ie</a> </li>
+        <li class="email"><a href="mailto: info@granite.ie" itemprop="email">info@granite.ie</a>
+        </li>
+        <li class="map"><a href="https://www.google.ie/maps?q=gray+office+park+map&amp;ion=1&amp;espv=2&amp;bav=on.2,or.r_cp.&amp;bvm=bv.102537793,d.ZGU&amp;biw=1366&amp;bih=628&amp;dpr=1&amp;um=1&amp;ie=UTF-8&amp;sa=X&amp;ved=0CAcQ_AUoAmoVChMIiNXV-sXvxwIVoaDbCh0ozgnN"
+            target="_blank">View Map</a> </li>
       </ul>
     </div>
     <div class="col-sm-4"></div>
@@ -69,16 +83,23 @@
   <div class="video">
     <video autoplay loop>
       <source src="./video/dancing-guy-YouTube480-SD-Wide-LowBitrate.mp4" type="video/mp4">
-      Your browser does not support the video tag. </video>
+      Your browser does not support the video tag.
+    </video>
   </div>
   <ul>
-    <li> <a href="./" class="home">Home</a> <a href="./portfolio" class="work">Work</a> <a href="./what-we-do" class="services">Services</a> <a href="./who-we-are" class="about">About</a> <a href="./careers" class="careers">Careers</a> </li>
+    <li>
+      <a href="./" class="home">Home</a>
+      <a href="./portfolio" class="work">Work</a>
+      <a href="./what-we-do" class="services">Services</a>
+      <a href="./who-we-are" class="about">About</a>
+      <a href="./careers" class="careers">Careers</a>
+    </li>
   </ul>
 </nav>
-<script type="text/javascript" src="./plugins/jquery-3.3.1/jquery-3.3.1.min.js"></script> 
-<script type="text/javascript" src="./plugins/slick/slick.min.js"></script> 
-<script type="text/javascript" src="./js/scripts.js"></script> 
-<!-- Copied from old site --> 
+<script type="text/javascript" src="./plugins/jquery-3.3.1/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="./plugins/slick/slick.min.js"></script>
+<script type="text/javascript" src="./js/scripts.js"></script>
+<!-- Copied from old site -->
 <script>
   (function(i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r;
@@ -100,9 +121,13 @@
   ga('create', 'UA-6450443-5', 'auto');
 
   ga('send', 'pageview');
-</script> 
+</script>
 
-<!--Start of Tawk.to Script--> 
+
+
+
+
+<!--Start of Tawk.to Script-->
 
 <script type="text/javascript">
   /*var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
@@ -122,9 +147,11 @@
     s0.parentNode.insertBefore(s1,s0);
 
     })();*/
-</script> 
+</script>
 
-<!--End of Tawk.to Script--> 
+
+
+<!--End of Tawk.to Script-->
 
 <script type="text/javascript">
   try {
@@ -138,9 +165,11 @@
 
 
   } catch (err) {}
-</script> 
+</script>
 
-<!-- Piwik --> 
+
+
+<!-- Piwik -->
 
 <script type="text/javascript">
   var _paq = _paq || [];
@@ -172,17 +201,20 @@
     s.parentNode.insertBefore(g, s);
 
   })();
-</script> 
+</script>
 
-<!-- End Piwik Code --> 
+<!-- End Piwik Code -->
 
-<!-- OneTrust Cookies Consent Notice start --> 
+<!-- OneTrust Cookies Consent Notice start -->
 
-<script src="https://cookiepro.blob.core.windows.net/consent/ca91b006-be6d-4359-a33d-03669b7bca67.js" type="text/javascript" charset="UTF-8"></script> 
+<script src="https://cookiepro.blob.core.windows.net/consent/ca91b006-be6d-4359-a33d-03669b7bca67.js" type="text/javascript" charset="UTF-8"></script>
+
 <script type="text/javascript">
   function OptanonWrapper() {}
-</script> 
+</script>
 
 <!-- OneTrust Cookies Consent Notice end -->
 
-</body></html>
+</body>
+
+</html>
