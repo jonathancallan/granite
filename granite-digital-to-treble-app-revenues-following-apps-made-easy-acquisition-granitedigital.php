b6bb918eec0d92d33e70db72107458e01a3b778f
Jonathan Callan <jonathan.callan@granite.ie>
2019-11-22 12:55:58 +0000

    Draft page for UX Workshop (no real content) News Apps Made Easy acquisition

---
+++ granite-digital-to-treble-app-revenues-following-apps-made-easy-acquisition-granitedigital.php
@@ -0,0 +1,63 @@
+<?php
+$metaTitle = "Granite Digital to treble app revenues following Apps Made Easy acquisition.";
+$metaDescription = "Granite Digital to treble app revenues following Apps Made Easy acquisition.";
+$metaKeywords = "Granite Digital to treble app revenues following Apps Made Easy acquisition.";
+$ogUrl = "https://www.granite.ie/granite-digital-to-treble-app-revenues-following-apps-made-easy-acquisition-granitedigital";
+?>
+<?php require 'inc/head.php'; ?>
+
+<body class="home standard">
+<div class="hero-background">
+  <svg id="angle-bar" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 639.26 414.13">
+    <polygon class="primary" points="0 280.89 133.24 414.13 547.37 0 280.89 0 0 280.89" />
+    <polygon class="secondary" points="388 191.6 417.83 221.44 639.26 0 579.61 0 388 191.6" />
+  </svg>
+</div>
+<?php require 'inc/header.php'; ?>
+<main> 
+  <!-- Hero banner -->
+  <section class="hero" style="margin-top: 10vh;">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col offset-sm-3">
+            <p style="margin-bottom: 3em;"><img src="./img/granite-to-treble-app-revenues-following-apps-made-easy-acquisition-3.jpg" alt="Granite to treble app revenues following Apps Made Easy acquisition"></p>
+          <h1>Granite Digital to treble app revenues following Apps Made Easy acquisition</h1>
+          <p>Granite Digital, Ireland's leading full-service digital agency, today announces that it has acquired Galway-based app development company, Apps Made Easy. The deal, which is part of an ambitious growth strategy for Granite Digital, will allow the Deloitte Fast 50 company to treble its app development revenues to €750,000 in the next 12 months.</p>
+            <p class="small-text">22 November 2019</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="offset-sm-3 col-sm-7 col-xl-6">
+          <p>Established in 2008, Granite Digital is a consulting group of more than 60 professionals providing Strategy, UX Design, Web solutions, SEO & managed hosting services to clients across a wide range of industries in the private and public sector. To meet growing market demand for the development of customer-focused mobile apps, the company has collaborated with award-winning Apps Made Easy on a number of projects for high profile clients including Fexco, Dublin City Council, UCC and Bon Secours Health System.</p>
+                      <figure> <img src="./img/granite-to-treble-app-revenues-following-apps-made-easy-acquisition-2.jpg" alt="Granite to treble app revenues following Apps Made Easy acquisition">
+            <figcaption>
+            Pictured announcing Granite Digital's acquisition of Apps Made Easy are (l-r) Robert Carpenter, founder, Granite; Conor Buckley, CEO, Granite; Alf Smiddy, Chairman, Granite; Niall Campbell, director, Apps Made Easy and Seamus White, co-founder, Granite.
+            </figcaption>
+          </figure>
+          <p>As the provider of digital solutions for clients, Granite identified the opportunity to expand and enhance its mobile application offering further through an acquisition. By acquiring Apps Made Easy, Granite Digital will be able to extend its mobile app development service and establish a new dedicated division that will produce user-oriented mobile solutions.</p>
+          <p>Granite Digital is on an ambitious growth path, with overall revenues expected to hit €5 million in 2019 and to reach €10 million by 2022. The end-to-end provider for clients wishing to transform their digital presence cites the digital transformation of the financial services sector as a key driver of this. Current clients in this sector include Davy, Fexco, Aon and The Central Bank.</p>
+          <p>The announcement will also see Granite Digital's geographical footprint expand to three locations in Ireland, adding Galway to its existing Dublin and Cork offices.</p>
+                      <figure> <img src="./img/granite-to-treble-app-revenues-following-apps-made-easy-acquisition-1.jpg" alt="Granite to treble app revenues following Apps Made Easy acquisition">
+            <figcaption>
+            Pictured announcing Granite Digital's acquisition of Apps Made Easy are (l-r) Robert Carpenter, Co-Founder of Granite Digital; and Niall Campbell, Director of Apps Made Easy.
+            </figcaption>
+          </figure>
+          <p>Robert Carpenter, Co-Founder of Granite Digital, said: "Apps Made Easy is an exciting new addition to the range of services that Granite Digital provides. This acquisition will transform our service offering, enabling us to establish ourselves as a one-stop-shop for every aspect of our clients' digital presence – whether that's Digital Strategy, UX Design, Applications Development, SEO or App development.</p>
+          <p>Niall Campbell, Director of Apps Made Easy said: "As one of Ireland's most established brands in web development, Granite Digital is an ideal partner for Apps Made Easy. Our complementary expertise means we can offer unique and unparalleled software solutions to clients. We are excited to see how Granite Digital and Apps Made Easy can continue to grow together in the future."</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+<div class="footer-cta bg-white">
+  <div class="box">
+    <h2>Let us help your Digital Marketing</h2>
+    <p>Contact us today <a href="tel:+35318512800">+ 353 (1) 6337979</a></p>
+  </div>
+</div>
+<footer class="bg-white">
+  <?php require 'inc/footer.php'; ?>
