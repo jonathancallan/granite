c3bbf97227dc92d0f3833aad43e3ef12e053202c
Jonathan Callan <jonathan.callan@granite.ie>
2019-11-29 15:16:51 +0000

    Update UX Workshop images

---
+++ umbraco.php
@@ -0,0 +1,62 @@
+<?php
+$metaTitle = "Umbraco - Granite Digital";
+$metaDescription = "Umbraco Granite Digital.";
+$metaKeywords = "Umbraco Cork, Ireland";
+$ogUrl = "https://www.granite.ie/umbraco";
+?>
+<?php require 'inc/head.php'; ?>
+<body class="services standard">
+<div class="hero-background mask">
+  <div id="mask"></div>
+  <video id="masked" autoplay loop>
+    <source src="../video/office-scene-slowmotion-YouTube480-SD-Wide-LowBitrate.mp4" type="video/mp4">
+    Your browser does not support the video tag. </video>
+</div>
+<?php require 'inc/header.php'; ?>
+<main> 
+  <!-- Hero banner -->
+  <div class="hero">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col offset-sm-2">
+          <h1>Umbraco</h1>
+          <p>Customer experience cuts across every interaction your audience has with you. Do it well — and you'll have a customer for life. We collaborate, understand and craft solutions to enhance your digital business..</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Start of main content -->
+  <section class="text-grid">
+    <div class="container-fluid">
+      <div class="row padding-b">
+        <div class="col-12 offset-md-2 col-md-4 col-lg-4">
+          <h3>Granite Digital are an Umbraco Gold Partner.</h3>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="text-grid">
+    <div class="container-fluid">
+      <div class="row padding-b">
+          <div class="col-12 offset-md-2 col-md-3 col-lg-2">
+              <p><img src="../img/umbraco-logo.png" alt="Umbraco" style="width: 200px;"></p>
+          </div>
+        <div class="col-12  col-md-6 col-lg-6">
+          <p>Granite Digital are passionate about our customers and how they delight their customers through digital experiences. We believe in the power and value of Umbraco and we're here to make your adoption and integration of this feature-driven CMS painless for your organisation.</p>
+            <p>We recognise the advantages of this powerful platform. We'll work with you to determine all of your needs. From web development and deployment to ongoing management, marketing, and strategy. We are your trusted partner in results-oriented, experience-driven services.</p>
+            <p>As a Umbraco Certified Gold Partner, you'll benefit from our vast knowledge of Umbraco and their latest materials. You will have direct access to the Umbraco HQ team, for support and advice on solutions, and best-practice methodologies.</p>
+        </div>
+      </div>
+      <img src="https://via.placeholder.com/1920x600" alt="" class="padding-b hidden"> </div>
+  </section>
+</main>
+<div class="footer-cta">
+  <div class="box">
+    <h2>Got more questions?</h2>
+    <p>We'd love to hear from you. Just
+      <call></call>
+      <a href="tel:+35318512800">+ 353 (1) 6337979</a></p>
+  </div>
+</div>
+<footer>
+  <?php require 'inc/footer.php'; ?>
---
+++ ux-training/ux-essentials.php
@@ -0,0 +1,129 @@
+<?php
+$metaTitle = "UX Essentials - UX training courses Dublin & Cork - Granite Digital";
+$metaDescription = "Get trained in UX by the people who practice it everyday. User Experience is the quality of the experience a person has when interacting with a specific design.";
+$metaKeywords = "UX training courses Dublin & Cork - Granite Digital";
+$ogUrl = "https://www.granite.ie/ux-training/ux-essentials";
+?>
+<?php require '../inc/head.php'; ?>
+<body class="services standard">
+<div class="hero-background mask">
+  <div id="mask"></div>
+  <video id="masked" autoplay loop>
+    <source src="../video/office-scene-slowmotion-YouTube480-SD-Wide-LowBitrate.mp4" type="video/mp4">
+    Your browser does not support the video tag. </video>
+</div>
+<?php require '../inc/header.php'; ?>
+<main> 
+  <!-- Hero banner -->
+  <div class="hero">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col offset-sm-2">
+          <h1>UX Essentials</h1>
+          <p>Granite will run their first UX Essentials workshop on the 26th of November which is an invite only event for their clients.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Start of main content -->
+  <section class="text-grid">
+    <div class="container-fluid">
+      <div class="row padding-b">
+        <div class="col-12 offset-md-2 col-md-4 col-lg-4">
+          <h3>"User Experience" is the quality of the experience a person has when interacting with a specific design.</h3>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="text-grid">
+    <div class="container-fluid">
+      <div class="row padding-b">
+        <div class="col-12 offset-md-2 col-md-4 col-lg-4">
+          <h2>UX workshop overview</h2>
+          <ul>
+            <li>What UX is and what is NOT</li>
+            <li>Hands-on exercise</li>
+            <li>UX Strategy &amp; Maturity levels</li>
+            <li>Experience Vision and why it's important</li>
+            <li>Industry Stats</li>
+            <li>UX Tools: Personas</li>
+            <li>Design Thinking: Empathy Mapping</li>
+            <li>Measuring Impact: Metrics, Analytics and KPIs vs User Frustrations</li>
+            <li>UX Tools: Journey Mapping</li>
+            <li>Workshop: your Product Empathy &amp; Journey Map</li>
+            <li>UX tools: Heuristic Evaluation</li>
+            <li>Exposure hours and the Kano Model</li>
+            <li>Workshop: User testing in real life</li>
+            <li>Features vs Problems to solve</li>
+            <li>Recap</li>
+            <li>Questions &amp; Answers</li>
+          </ul>
+        </div>
+        <div class="col-12 col-md-4 offset-lg-1 col-lg-4 padding-b">
+          <h2>Who should attend?</h2>
+          <ul>
+            <li>Marketing Professionals</li>
+            <li>Product Team members</li>
+            <li>Project Managers/Product Owners</li>
+            <li>Designers</li>
+            <li>Researchers</li>
+            <li>C-level executives</li>
+          </ul>
+        </div>
+      </div>
+      <img src="img/ux-essentials-workshop.jpg" alt="Radu speaking at the Granite UX Essentials Workshop" class="padding-b"> </div>
+  </section>
+  <section class="divider">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col offset-sm-2 padding-b">
+          <h2>Meet the UX team</h2>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="image-grid">
+    <div class="container-fluid">
+      <div class="row padding-b">
+        <div class="offset-sm-2 col-sm-3 col-md-3 col-xl-4">
+          <div class="thumbnail"> <a href="#"><img src="./img/radu-jita.jpg" alt="Radu Jitea"></a> </div>
+          <h2><span class="small">Lead UX</span>Radu Jitea</h2>
+          <p>I'm a UX product designer based in Dublin, and my goal is to find out what makes people tick. On a continuous quest to expand my knowledge and to identify blind spots and biases, I spent my childhood drawing on every surface available and later discovered a passion for Maths and Physics. I then progressed to the next "logical" step of pursuing a Law degree. After 3 years of being the lousiest law student, I decided to return to my childhood interests, colour markers and sticky notes. I went on to complete a postgraduate in Design Studies and a Master in UX. For the past 10 years, I've been helping businesses, big &amp; small, focus on the needs of their customers and design delightful experiences.</p>
+        </div>
+        <div class="col-sm-3 col-md-3 col-xl-4">
+          <div class="thumbnail"> <a href="#"><img src="./img/treasa-flynn.jpg" alt="Treasa Flynn"></a> </div>
+          <h2><span class="small">Senior UX/UI Designer</span>Treasa Flynn</h2>
+          <p>Treasa believes the best products are designed when the end user is central to the decision making. Without research, questioning the status quo or asking what the end user's actual requirements are, then it is unlikely that the end result will deliver successful results for our clients. Good design and frictionless user experience is a minimum requirement if an organisation is to flourish online in a challenging digital market place.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+    <section class="divider">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col offset-sm-2 padding-b">
+          <h2>More information</h2>
+        </div>
+      </div>
+    </div>
+  </section>
+      <section class="text-grid">
+    <div class="container-fluid">
+      <div class="row padding-b">
+        <div class="col-12 offset-md-2 col-md-4 col-lg-6">
+          <p>It will start at 9:30 (coffee from 9) and we will run until 3pm with a break of 40 mins for sandwiches/refreshments (we will supply).  There will be time for 1-1 and Q+A after if required.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+<div class="footer-cta">
+  <div class="box">
+    <h2>Got more questions?</h2>
+    <p>We'd love to hear from you. Just
+      <call></call>
+      <a href="tel:+35318512800">+ 353 (1) 6337979</a></p>
+  </div>
+</div>
+<footer>
+  <?php require '../inc/footer.php'; ?>
