25623e94449da7c66e39f3e759396cc0425a2ba1
Jonathan Callan <jonathan.callan@granite.ie>
2020-03-13 17:04:35 +0000

    Create placeholder for news on home page (hidden until Gary is ready with the WP install). Added COVID-19 notification to home page.

---
+++ covid-19-granite-business-continuity-plan-fully-operational.php
@@ -0,0 +1,116 @@
+<?php
+$metaTitle = "Granite Digital to create 50 new jobs in Dublin, Cork and Galway.";
+$metaDescription = "Granite Digital to create 50 new jobs in Dublin, Cork and Galway.";
+$metaKeywords = "Granite Digital to create 50 new jobs in Dublin, Cork and Galway.";
+$ogUrl = "https://www.granite.ie/granite-digital-to-create-50-new-jobs-in-dublin-cork-galway";
+$ogImage = "https://www.granite.ie/img/rob-carpenter-seamus-white-conor-buckley-1200x630.jpg";
+?>
+<?php require 'inc/head.php'; ?>
+
+<body class="notification standard hide-notification">
+<div class="hero-background">
+  <svg id="angle-bar" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 639.26 414.13">
+    <polygon class="primary" points="0 280.89 133.24 414.13 547.37 0 280.89 0 0 280.89" />
+    <polygon class="secondary" points="388 191.6 417.83 221.44 639.26 0 579.61 0 388 191.6" />
+  </svg>
+</div>
+<?php require 'inc/header.php'; ?>
+<main> 
+  <!-- Hero banner -->
+  <section class="hero" style="margin-top: 10vh;">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col offset-sm-3 col-xl-6">
+          <h1>Covid-19 Granite Business Continuity Plan</h1>
+          <h2 style="margin-bottom: 40px;">We are 100% operational</h2>
+          <p>We know your digital business operations will be reliant on Granite providing you with reliable and uninterrupted service and with this in mind we would like to share with you the measures we are taking to ensure we are 100% operational.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="offset-sm-3 col-sm-7 col-xl-6">
+          <h2>Decisive action</h2>
+          <p>Yesterday Granite made the decision to mobilise our team to work from home. We have robust plans in place to ensure that we can continue to serve our customers and we welcome the measures announced by the Government in an effort to limit the spread of the COVID-19 virus.</p>
+          <p>Granite is well placed to be fully operational during this crisis, our multiple office locations in Ireland and other EU countries mean that everyday we work with colleagues in dispersed locations without a thought. This is now second nature to us and we have also shared some of the tools we use in the hope it may help you get your business operational faster if you are impacted by moving from your normal location to home.</p>
+          <p>Given the current outbreak of COVID-19 and the impact the virus is having on our customers, employees and families, we would like to share our Business Continuity plans with you.
+            Despite the uncertainty of COVID-19, we expect to continue to deliver the same level of service excellence to our customers and have the following measures in place to ensure your success:</p>
+          <h3>Availability of our Amazing Team</h3>
+          <p>Granite is a company with customers in over 15 countries and a team of over 60 across 5 countries so our team is already accustomed to collaborating across diverse locations and is able to easily connect with each other and our customers in a secure and productive environment.</p>
+          <p>We will be available to work with you and deliver on your projects, regardless of our physical work locations.</p>
+          <h3>Availability of our Technical Solutions</h3>
+          <p>Our Solutions are secure, robust and resilient, sitting exclusively in the Cloud with two factor authentication in place for secure access. We have 99.99% reliability and our solutions have automatic backup and redundancy functions in place as standard. We also have a world class support team who are available for, in and out of hours, support as you need us.</p>
+          <h3>Key Contact Numbers Day or Night</h3>
+          <p>We understand everyone will be concerned for the health of their teams and their business. We are here to help in any way we can. Working remotely will become difficult and we are happy to take calls late into the evening for you, with kids or pets as optional :) </p>
+          <p>You are most likely already working with one of the people below in some element of your project.</p>
+          <ul>
+            <li>Conor Buckley <a href="tel:0868143771">(086) 814 3771</a></li>
+            <li>Seamus White <a href="tel:0878258222">(087) 825 8222</a></li>
+            <li>Gary Corcoran <a href="tel:0864023704">(086) 402 3704</a></li>
+            <li>Ger O'Shea <a href="tel:0862303334">(086) 230 3334</a></li>
+            <li>Sean Byrne <a href="tel:0872358447">(087) 235 8447</a></li>
+            <li>Alan Henley <a href="tel:0871304538">(087) 130 4538</a></li>
+            <li>Jon Callan <a href="tel:0872020420">(087) 202 0420</a></li>
+            <li>Dympna Leahy <a href="tel:087 2065917">(087) 2065917</a></li>
+            <li>Rob Carpenter <a href="tel:0879530159">087 9530159</a></li>
+          </ul>
+          <p>Our Support team can always be contacted Mon-Fri on <a href="tel:0212427890">(021) 2427890</a> or <a href="tel:01 6337979">(01) 6337979</a></p>
+          <h2>Some key things we are doing</h2>
+          <ul>
+            <li>We have enabled remote working for our entire team and have ensured everyone can work from home productively and securely for a longer period of time.</li>
+            <li>We are providing daily updates to our team via email and slack.</li>
+            <li>Management is meeting online twice a day trying to ensure we keep business running as usual where possible for customers and staff. We have a Covid-19 ops team that meets also every morning remotely via video conference.</li>
+            <li>We have suspended non-essential business travel for our employees and prohibited business travel to any affected areas.</li>
+            <li>Leadership teams are working closely together ensuring we have a common map and understanding of the situation and priorities.</li>
+            <li>All decisions and actions are being logged.</li>
+            <li>All scheduled meetings (Internal and external) are being facilitated through video conferencing.</li>
+            <li>We are collaborating using shared documents while on the phone or online call, as a replacement for sitting next to each other.</li>
+            <li>Slack is used as normal, but with an emphasis on keeping everyone informed about what is going on regarding our now virtual workplace.</li>
+            <li>We are making ourselves available to our customers out of hours as we know this will be far from standard business as usual.</li>
+          </ul>
+          <h2>Some of the tools we use everyday to work remotely</h2>
+          <p>We recognise that some of our clients are not accustomed to remote working and may be struggling with the challenges it presents. Here is a list of some of the tools we use that may be appropriate for your business. Let us know if you need any advice.</p>
+          <h3>Communications</h3>
+          <ul>
+            <li><a href="https://slack.com/" target="_blank">Slack</a> (Internal and external)</li>
+            <li><a href="https://zoom.us/" target="_blank">Zoom</a> (External)</li>
+            <li>Google Hangouts (Internal and external)</li>
+            <li>VOIP Phone System (Phone System over the Internet)</li>
+          </ul>
+          <h3>Collaboration</h3>
+          <ul>
+            <li><a href="https://www.teamwork.com/" target="_blank">Teamwork.com</a> (Projects)</li>
+            <li><a href="https://www.teamwork.com/desk/" target="_blank">TeamworkDesk</a> (Support)</li>
+            <li>Google Docs (Document sharing and editing)</li>
+            <li>Google Sheets (Spreadsheets sharing and editing)</li>
+            <li><a href="https://www.smartsheet.com/" target="_blank">Smartsheets</a> (Project Plan and Team KPIs)</li>
+            <li><a href="https://trello.com/" target="_blank">Trello</a> (Kanban task-lists)</li>
+            <li><a href="https://www.figma.com/f" target="_blank">Figma</a> (Collaborative platform for Designers to work together)</li>
+            <li><a href="https://www.dropbox.com/" target="_blank">Dropbox</a> and <a href="https://www.google.com/drive/" target="_blank">Google Drive</a> for file sharing</li>
+          </ul>
+          <h3>Remote Access to Computers</h3>
+          <ul>
+            <li><a href="https://remotedesktop.google.com/" target="_blank">Chrome Remote Desktop</a></li>
+            <li><a href="https://www.teamviewer.com/en/" target="_blank">Teamviewer</a></li>
+          </ul>
+          <h3>Troubleshooting</h3>
+          <ul>
+            <li><a href="https://supportdetails.com/" target="_blank">Supportdetails.com</a> (providing their OS/browser/system info for troubleshooting issues)</li>
+            <li><a href="https://tinytake.com/" target="_blank">Tinytake.com</a> (Screen capture and video recording)</li>
+          </ul>
+          <p>Thank you for working with Granite and we remain as committed as ever to ensuring your success. We wish you, your colleagues and families the very best during these challenging times.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+<div class="footer-cta bg-white">
+  <div class="box" style="background-color: #FEE101; color: #000;">
+    <h2>Contact us if you have any further questions</h2>
+    <p>Email our <a href="mailto:support@granite.ie" style="color: #000; text-decoration: underline;">Support Team</a> or phone the contact numbers listed above.</p>
+  </div>
+</div>
+<footer class="bg-white">
+  <?php require 'inc/footer.php'; ?>
--- index.php
+++ index.php
@@ -7,6 +7,18 @@ $ogUrl = "https://www.granite.ie/";
 <?php require 'inc/head.php' ?>
 
 <body class="home">
+        <section class="notification">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-1 hidden-mobile"><img src="./img/icon-virus.svg" alt="" style="max-width: 60px;"></div>
+                <div class="col-lg-11">
+                    <h2><b>Our COVID-19 Business Continuity Plan</b></h2>
+        <p class="hidden-mobile">We know your digital business operations will be reliant on Granite providing you with reliable and uninterrupted service and with this in mind we would like to share with you the measures we are taking to ensure we are 100% operational.</p>
+                    <p><a href="./covid-19-granite-business-continuity-plan-fully-operational" class="icon forward">Find out more</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
 <?php require 'inc/header.php'; ?>
 <main>
   <div class="slider-wrapper">
@@ -22,7 +34,62 @@ $ogUrl = "https://www.granite.ie/";
       <div class="item careers"><a href="./careers"><b>Careers</b></a> </div>
     </div>
   </div>
-  <section class="client-logo-slider">
+    <section class="content news hidden">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h2><a href="">Granite partners with Dobiquity to create COVID-19 checklist</a></h2>
+                    <h4>13 March 2020</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error cum, dolor esse fuga aperiam modi velit dignissimos reiciendis voluptatibus nam. Sapiente cupiditate quis quia explicabo quos, non dignissimos voluptatibus perspiciatis.</p>
+                    <p><a href="#" class="icon forward">Read the full story</a></p>
+                </div>
+                <div class="col-lg-6">
+                    <h2><a href="#">Another news article heading here.</a></h2>
+                    <h4>14 March 2020</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor modi, reprehenderit impedit laboriosam. Odio consequuntur iusto suscipit enim autem repudiandae consequatur at! Aliquid quod laborum pariatur quidem corrupti quam consequatur.</p>
+                    <p><a href="#" class="icon forward">Read the full story</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="container" style="margin-top: 40px;">
+            <div class="row">
+                <div class="col">
+                    <p class="margin-zero"><a href="#" class="button">View all news</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+  <section class="content grid bg-white padding-t margin-b-zero" style="margin-top:0; padding-bottom: 50px;">
+    <div class="container-fluid">
+      <div class="row">
+          <div class="col-lg-6 col-xl-3">
+          <h2><a href="./granite-digital-to-create-50-new-jobs-in-dublin-cork-galway.php">50 New Jobs!</a></h2>
+          <h3>We're hiring in Dublin, Cork and Galway</h3>
+          <p>The jobs are part of a &euro;2.5 million investment to grow revenue from €5 million to €10 million by 2022.</p>
+          <p><a href="./granite-digital-to-create-50-new-jobs-in-dublin-cork-galway.php" class="icon forward">Find out more</a></p>
+        </div>
+        <div class="col-lg-6 col-xl-3">
+          <h2><a href="./academy.php">Our Academy</a></h2>
+          <h3>Professional development.</h3>
+          <p>Immerse yourself in the digital world, as well as provide opportunities for personal and professional development.</p>
+          <p><a href="./academy.php" class="icon forward">Find out more</a></p>
+        </div>
+        <div class="col-lg-6 col-xl-3">
+          <h2><a href="./ux-training/ux-essentials.php">UX Training</a></h2>
+          <h3>Client Workshops</h3>
+          <p>Our UX Essentials workshop is an invite only event for Granite clients.</p>
+          <p><a href="./ux-training/ux-essentials.php" class="icon forward">Find out more</a></p>
+        </div>
+        <div class="col-lg-6 col-xl-3">
+          <h2><a href="./what-we-do.php">Digital agency</a></h2>
+          <h3>Design, Development &amp; Marketing</h3>
+          <p>Our team of experts will simplify the process for you, making it a jargon free experience and ensuring you create the best face for your business online.</p>
+          <p><a href="./what-we-do.php" class="icon forward">Find out more</a></p>
+        </div>
+      </div>
+    </div>
+  </section>
+          <section class="client-logo-slider">
     <div class="item"><img src="./img/dalata.png" alt="Dalata Hotel Group"></div>
     <div class="item"><img src="./img/davy.png" alt="Davy"></div>
     <div class="item"><img src="./img/ucd.png" alt="UCD"></div>
@@ -34,36 +101,6 @@ $ogUrl = "https://www.granite.ie/";
     <div class="item"><img src="./img/dublin-bus.png" alt="Dublin Bus"></div>
     <div class="item"><img src="./img/bon-secours.png" alt="Bon Secours"></div>
     <div class="item"><img src="./img/forsa.png" alt="Forsa"></div>
-  </section>
-  <section class="content grid bg-white padding-t margin-b-zero" style="margin-top:0; padding-bottom: 50px;">
-    <div class="container-fluid">
-      <div class="row">
-          <div class="col-lg-6 col-xl-3">
-          <h2>50 New Jobs!</h2>
-          <h3><a href="./granite-digital-to-create-50-new-jobs-in-dublin-cork-galway.php">We're hiring in Dublin, Cork and Galway</a></h3>
-          <p>The jobs are part of a &euro;2.5 million investment to grow revenue from €5 million to €10 million by 2022.</p>
-          <p><a href="./granite-digital-to-create-50-new-jobs-in-dublin-cork-galway.php" class="icon forward">Find out more</a></p>
-        </div>
-        <div class="col-lg-6 col-xl-3">
-          <h2>Our Academy</h2>
-          <h3><a href="./academy.php">Opportunities for personal and professional development. </a></h3>
-          <p>Our academy is designed to immerse you in the digital world, as well as provide opportunities for personal and professional development.</p>
-          <p><a href="./academy.php" class="icon forward">Find out more</a></p>
-        </div>
-        <div class="col-lg-6 col-xl-3">
-          <h2>UX Essentials Training</h2>
-          <h3><a href="./ux-training/ux-essentials.php">Client Workshops</a></h3>
-          <p>Our UX Essentials workshop is an invite only event for Granite clients.</p>
-          <p><a href="./ux-training/ux-essentials.php" class="icon forward">Find out more</a></p>
-        </div>
-        <div class="col-lg-6 col-xl-3">
-          <h2>Digital agency</h2>
-          <h3><a href="./what-we-do.php">Design, Development &amp; Marketing</a></h3>
-          <p>Our team of experts will simplify the process for you, making it a jargon free experience and ensuring you create the best face for your business online.</p>
-          <p><a href="./what-we-do.php" class="icon forward">Find out more</a></p>
-        </div>
-      </div>
-    </div>
   </section>
 </main>
 <footer class="">
